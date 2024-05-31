32f7ddd81da64370ea2e894b8f835e05bf00ca16
sarananan <168821539+sarananan@users.noreply.github.com>
2024-05-31 13:20:57 +0530

    Add files via upload

---
+++ header/script.js
@@ -0,0 +1,19 @@
+let hamburger=document.getElementById("burger");
+let medianav=document.querySelector(".hambuger-menu-list");
+let auto1=document.querySelector(".auto1");
+hamburger.addEventListener("click",show)
+function show(){
+    medianav.classList.toggle("togglenav");
+let num=100;
+}
+function left(){
+    lbtn.style.marginLeft=`0-${num}`;
+}
+let lbtn=document.getElementById("radio1");
+let rbtn=document.getElementById("radio3");
+function left(){
+ lbtn.checked=true;
+}
+function right(){
+    rbtn.checked=true;
+}
